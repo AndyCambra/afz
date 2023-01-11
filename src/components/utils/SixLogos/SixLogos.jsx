f7a474ed5fd1d8d39a62b567c819e2851f957d1a
AndyCambra <andycambra@gmail.com>
2023-01-11 19:08:03 -0300

    Reaseguradores Logos

--- src/components/utils/SixLogos/SixLogos.jsx
+++ src/components/utils/SixLogos/SixLogos.jsx
@@ -6,13 +6,32 @@ const SixLogos = () => {
     <div className='six-logos-start'> 
           
     <div className='six-logos1'>
-     {/*  <img src="img/logoAfz.png" alt="logo"/> */}
+      <img src="img/LogosRe1.jpg" alt="Sompo Internacional"/>
     </div>
-    <div className='six-logos2'>2</div> 
-    <div className='six-logos3'>3</div>
-    <div className='six-logos4'>4</div>
-    <div className='six-logos5'>5</div>
-    <div className='six-logos6'>6</div>
+    <div className='six-logos2'>
+    <img src="img/LogosRe2.jpg" alt="Liberty Mutual"/>
+    </div> 
+    <div className='six-logos3'>
+    <img src="img/LogosRe3.jpg" alt="Hannover re"/>
+    </div>
+    <div className='six-logos4'>
+    <img src="img/LogosRe4.jpg" alt="Axis"/>
+    </div>
+    <div className='six-logos5'>
+    <img src="img/LogosRe5.jpg" alt="Validus re"/>
+    </div>
+    <div className='six-logos6'>
+    <img src="img/LogosRe6.jpg" alt="Atradius"/>
+    </div>
+    <div className='six-logos7'>
+    <img src="img/LogosRe7.jpg" alt="Coface"/>
+    </div>
+    <div className='six-logos8'>
+    <img src="img/LogosRe8.jpg" alt="Credendo"/>
+    </div>
+    <div className='six-logos9'>
+    <img src="img/LogosRe9.jpg" alt="Hamilton"/>
+    </div>
    
   </div>
   )
