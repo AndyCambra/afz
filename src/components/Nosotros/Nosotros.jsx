6a504b031104093366d4e22437a2332c1428c7a6
AndyCambra <andycambra@gmail.com>
2023-01-11 09:21:50 -0300

    Ref

--- src/components/Nosotros/Nosotros.jsx
+++ src/components/Nosotros/Nosotros.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import HorizontalSubMenu from '../utils/HorizontalSubmenu/HorizontalSubMenu'
 import Staff from './Staff'
+import { useRef, useEffect } from 'react'
 
 import './nosotros.css'
 
 
 const Nosotros = () => {
-
+  const inRef= React.createRef()
+  console.log(inRef)
   const us=
       {title: "LA EMPRESA",
       text:'Somos una compañía especialista en Seguros de Caución que reúne experiencia e innovación. Brindamos las mejores herramientas para satisfacer la demanda de Productores, Tomadores y Asegurados en un mercado dinámico y competitivo.'
@@ -14,15 +16,14 @@ const Nosotros = () => {
  
   return (
     <div>
-      <div className='us-backgound'>
-       
+      <section ref={inRef}  title="us-backgound" id="section1" className='us-backgound' >
         <div className='grid'>
           <div className="us-block">
           <HorizontalSubMenu title={us.title} text={us.text} />
           </div>
         </div>
        
-      </div>
+      </section>
       <Staff />
      
     </div>
--- src/components/utils/HorizontalSubmenu/HorizontalSubMenu.jsx
+++ src/components/utils/HorizontalSubmenu/HorizontalSubMenu.jsx
@@ -1,27 +1,35 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React, { forwardRef } from 'react'
+import { Link } from 'react-router-dom'
 import './horizontalSubMenu.css'
+import { useState } from 'react'
 
-const HorizontalSubMenu = ({title, text}) => {
+const HorizontalSubMenu = forwardRef(({title, text},ref) => {
+  const [isActive, setIsActive]=useState(false)
+
+ const handleClick=() => {
+  setIsActive(!isActive)
+  } 
   return (
     <>
         <div className='subMenuBox'>
+            <Link to={ref={ref}} onClick={handleClick}>
             <h5>EMPRESA</h5>
+            </Link>
             <h5>|</h5>
             <h5>EQUIPO</h5>
             <h5>|</h5>
             <h5>REASEGURADORES</h5>
             <h5>|</h5>
             <h5>COMPROMISO</h5>
-            <NavLink to="/">
+            <Link to="/">
             <h5>HOME</h5>
-            </NavLink>
+            </Link>
         </div>
         <h5 className='horizontalTitle'>{title}</h5>
         <p className='horizontalText'>{text}</p>
         
         </>
   )
-}
+})
 
 export default HorizontalSubMenu
