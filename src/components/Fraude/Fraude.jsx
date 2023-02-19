ef97e61c75f65c5f0b8c6f368baac4d3fb3531c5
AndyCambra <andycambra@gmail.com>
2023-02-19 13:13:24 -0300

    fixes in Fraude and FraudMenu

--- src/components/Fraude/Fraude.jsx
+++ src/components/Fraude/Fraude.jsx
@@ -1,30 +1,30 @@
 import React from 'react'
 import FraudMenu from '../utils/FraudMenu/FraudMenu'
 import './fraude.css'
-import HorizontalPhoto from '../utils/HorizontalPhoto/HorizontalPhoto'
 import FirstFraudBox from '../utils/FraudBoxes/FirstFraudBox'
 import { useState, useEffect } from 'react'
 import SecondFraudBox from '../utils/FraudBoxes/SecondFraudBox'
 
 
 const Fraude = () => {
-const [isShow, setIsShow] = useState(true);
-const [isShow2, setIsShow2] = useState(false);
+const [selectNumber, setSelectNumber] = useState(1);
 
-const handleClick = () => {
-    setIsShow(true)
-    setIsShow2(false)
-}
-const handleClick2 = () => {
-    setIsShow(false)
-    setIsShow2(true)
-}
 
+const handleClick = i => {
+    setSelectNumber(i)
+}
+const selectComponent=()=>{
+  if(selectNumber===1){
+    return <FirstFraudBox />
+  }else{
+    return <SecondFraudBox />
+  }
+}
 useEffect(()=>{
     window.scrollTo(0,0);
   },[])
 
-console.log(1,  isShow, isShow2)
+console.log(1,  selectNumber)
 
   return (
     <div className='fraud-background'>
@@ -34,14 +34,10 @@ console.log(1,  isShow, isShow2)
         <h5 className='digital-title'>PREVENCIÓN DEL FRAUDE</h5>
         </div>
         <div className='fraud-menu'>
-        <FraudMenu handleClick={handleClick} handleClick2={handleClick2} className={isShow? "menu-link-selected":"menu-link"} className2={isShow2? "menu-link-selected":"menu-link"}/> 
+        <FraudMenu handleClick={handleClick} className1={selectNumber ===1? "menu-link-selected":"menu-link"} className2={selectNumber===2? "menu-link-selected":"menu-link"} /> 
         </div>
-      {isShow ? 
-      <FirstFraudBox /> : 
-      <SecondFraudBox />}  
+      {selectComponent()}
     </div>
-   {/*  {isShow ?
-    <HorizontalPhoto img='/img/DescargarForm.jpg' alt="Prevención del Fraude" />: null} */}
     </div> 
     </div>
   )
--- src/components/utils/FraudMenu/FraudMenu.jsx
+++ src/components/utils/FraudMenu/FraudMenu.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const FraudMenu = ({handleClick, handleClick2, className, className2}) => {
+const FraudMenu = ({handleClick, className1, className2}) => {
   return (
     <div className='menu-box'>
-    <Link className={className}  onClick={()=>handleClick()}>LA POLÍTICA ANTIFRAUDE Y SU INFLUENCIA EN LA COMPETITIVIDAD</Link>
-    <Link className={className2}  onClick={()=>handleClick2()}>CLAVES PARA EVITAR EL FRAUDE</Link>
+    <Link className={className1}  onClick={()=>handleClick(1)}>LA POLÍTICA ANTIFRAUDE Y SU INFLUENCIA EN LA COMPETITIVIDAD</Link>
+    <Link className={className2}  onClick={()=>handleClick(2)}>CLAVES PARA EVITAR EL FRAUDE</Link>
     <Link to='/' className='home-menu-link'>HOME</Link>
     </div>
   )
