05c86211730a9a185aacbd4cccbf53758f72f4be
AndyCambra <andycambra@gmail.com>
2023-02-24 18:16:44 -0300

    fixes in Cotizador Alqu, input number handleCancel

--- src/components/CotizadorAlquiler/CotizadorForm.jsx
+++ src/components/CotizadorAlquiler/CotizadorForm.jsx
@@ -7,10 +7,10 @@ const CotizadorForm = ({handleChange, handleSubmit, rentData}) => {
     <p className='input-explain'>Ingresá el monto sin puntos ni comas.</p>
     <p className='rent-label'>Alquiler mensual</p>
     <i className='i'>$</i>
-        <input name="rent" className="light-input-rent" value={rentData.rent} type="text" onChange={handleChange} required={true} autoComplete="off" />
+        <input name="rent" className="light-input-rent" value={rentData.rent} type="number" onChange={handleChange} required={true} />
     <p className='rent-label'>Expensas</p>
     <i className='i'>$</i>
-        <input name="expenses" value={rentData.expenses} id="prueba" className='light-input-rent' type="text" onChange={handleChange} required={true} autoComplete="off" /> 
+        <input name="expenses" value={rentData.expenses} id="prueba" className='light-input-rent' type="number" onChange={handleChange} required={true}  /> 
     <div className='radio-box'>
         <p className='rent-radio-title'>Duración:</p>
         <p className='rent-label'>2 años</p>
