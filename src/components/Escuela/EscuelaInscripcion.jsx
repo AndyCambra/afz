2bb815b01f866e4fde986596abb14e7366f7e20a
AndyCambra <andycambra@gmail.com>
2023-02-28 11:55:04 -0300

    EscuelaIncripcion validation filds

--- src/components/Escuela/EscuelaInscripcion.jsx
+++ src/components/Escuela/EscuelaInscripcion.jsx
@@ -5,7 +5,7 @@ import { useState } from 'react'
 import BigButton from '../utils/BigButton/BigButton'
 
 const EscuelaInscripcion = () => {
-  let [infoinput, setInfoInput]=useState({
+  let [infoInput, setinfoInput]=useState({
     userName: "",
     userLastName: "",
     company: "",
@@ -15,18 +15,72 @@ const EscuelaInscripcion = () => {
     course:""
   })
   const [sent, setSent]= useState(false)
+  const [errors, setErrors]= useState({})
 
-  
-  const handleChange=(e)=>{
+  const handleChange = (e, name, type, required = false, maxLength = false, minLength = false) => {
     const value= e.target.value
-  setInfoInput({...infoinput, [e.target.name]:value});
-  } 
+    const changedinfoInput = { ...infoInput, [e.target.name]:value };
+    const err = { ...errors }
+    const filterMail = /.*@[a-z0-9.-]*/i;
+    switch (type) {
+      case 'text':
+        changedinfoInput[name] = e.target.value;
+          if(required) {
+            err[name] = e.target.value ? false : 'El campo es requerido';
+          }
+          if(maxLength && !err[name]) {
+            err[name] = e.target.value.length > maxLength ? `El campo debe tener hasta ${maxLength} caracteres` : false;
+          }
+          if(minLength && !err[name]) {
+            err[name] = e.target.value.length < minLength ? `El campo debe tener más de ${minLength} caracteres` : false;
+          }
+          break;
+      case 'number':
+        changedinfoInput[name] = e.target.value;
+          if(required) {
+            err[name] = e.target.value ? false : 'El campo es requerido';
+          }
+          if(maxLength && !err[name]) {
+            err[name] = e.target.value.length > maxLength ? `El campo debe tener hasta ${maxLength} caracteres` : false;
+          }
+          if(minLength && !err[name]) {
+            err[name] = e.target.value.length < minLength ? `El campo debe tener más de ${minLength} caracteres` : false;
+          }
+          break;
+       case 'email':
+        changedinfoInput[name] = e.target.value;
+          if(required) {
+            err[name] = e.target.value ? false : 'El campo es requerido';
+          }
+          if(maxLength && !err[name]) {
+            err[name] = e.target.value.length > maxLength ? `El campo debe tener hasta ${maxLength} caracteres` : false;
+          }
+          if(minLength && !err[name]) {
+            err[name] = e.target.value.length < minLength ? `El campo debe tener más de ${minLength} caracteres` : false;
+          }
+          if(filterMail.test(value) === false){
+            err[name] = 'Ingrese un mail válido'
+          }
+          break;
+    
+          default:
+          break;
+        }
+        setinfoInput({...changedinfoInput, [e.target.name]:value});
+        setErrors(err);
+      }
+  //console.log(errors, infoInput)
+
+  /* const handleChange=(e)=>{
+    const value= e.target.value
+  setinfoInput({...infoInput, [e.target.name]:value});
+  }  */
   const handleSubmit=(e)=>{
     e.preventDefault();
     setSent(true)
 
   }
-console.log(1, infoinput)
+
 
 
   return (
@@ -51,16 +105,34 @@ console.log(1, infoinput)
             </div> 
           </div>
           <div className='inscription-title'>
-            {sent===true ? <div><h2>Gracias {infoinput.userName} por inscribirte!</h2><p>Te llegará en breve un mail de confirmación.</p></div>:<h2>COMPLETÁ EL FORMULARIO</h2>}
+            {sent===true ? <div><h2>Gracias {infoInput.userName} por inscribirte!</h2><p>Te llegará en breve un mail de confirmación.</p></div>:<h2>COMPLETÁ EL FORMULARIO</h2>}
           </div>  
             <form className= {sent===true ? 'display-none':'inscription-form'} onSubmit={handleSubmit}>
-            <input name="userName" className="light-input" value={infoinput.userName} type="text" onChange={handleChange} placeholder="Nombre" required={true}/>
-            <input name="userLastName" value={infoinput.userLastName} className='light-input' type="text" onChange={handleChange} placeholder='Apellido' required={true}/> 
-            <input name="company" className="light-input" value={infoinput.company} type="text" onChange={handleChange} placeholder="Organización a la que pertenece" required={true}/>
-            <input name="email" className="light-input" value={infoinput.email} type="email" onChange={handleChange} placeholder="Email" required={true}/>
-            <input name="phone" className="light-input" value={infoinput.phone} type="text" onChange={handleChange} placeholder="Teléfono" required={true}/>
-            <input name="dni" className="light-input" value={infoinput.dni} type="text" onChange={handleChange} placeholder="DNI" required={true}/>
-            <select name="course" className="light-input" value={infoinput.course} type="list" onChange={handleChange} placeholder="Curso" required={true}>
+            <div className='input-box'>
+              <input name="userName" className="light-input" value={infoInput.userName} type="text" onChange={ e => handleChange(e, 'userName', 'text', true, 75, 3)} placeholder="Nombre" required={true}/>
+              {errors['userName'] && <p className='error'>{errors['userName']}</p>}
+            </div>
+            <div className='input-box'>
+              <input name="userLastName" value={infoInput.userLastName} className='light-input' type="text" onChange={e => handleChange(e, 'userLastName', 'text', true, 75, 3)} placeholder='Apellido' required={true}/> 
+              {errors['userLastName'] && <p className='error'>{errors['userLastName']}</p>}
+            </div>
+            <div className='input-box'>
+              <input name="company" className="light-input" value={infoInput.company} type="text" onChange={e => handleChange(e, 'company', 'text', true, 75, 2)} placeholder="Organización a la que pertenece" required={true}/>
+              {errors['company'] && <p className='error'>{errors['company']}</p>}
+            </div>
+            <div className='input-box'>
+              <input name="email" className="light-input" value={infoInput.email} type="email" onChange={e => handleChange(e, 'email', 'email', true, 75, 8)} placeholder="Email" required={true}/>
+              {errors['email'] && <p className='error'>{errors['email']}</p>}
+            </div>
+            <div className='input-box'>
+              <input name="phone" className="light-input" value={infoInput.phone} type="number" onChange={e => handleChange(e, 'phone', 'number', true, 15, 7)} placeholder="Teléfono" required={true}/>
+              {errors['phone'] && <p className='error'>{errors['phone']}</p>}
+            </div>
+            <div className='input-box'>
+              <input name="dni" className="light-input" value={infoInput.dni} type="number" onChange={e => handleChange(e, 'dni', 'number', true, 15, 7)} placeholder="DNI" required={true}/>
+              {errors['dni'] && <p className='error'>{errors['dni']}</p>}
+            </div>
+            <select name="course" className="light-input" value={infoInput.course} type="list" onChange={handleChange} placeholder="Curso" required={true}>
               <option value="Introducción al Seguro de Caución">Introducción al Seguro de Caución</option>
               <option value="Cobranza y Refacturación">Cobranza y Refacturación</option>
               <option value="Suscripción de Riesgos">Suscripción de Riesgos</option>
@@ -71,7 +143,7 @@ console.log(1, infoinput)
             </div>
             </form>
           </div>
-        </div>
+        </div>s
       </section>
   )
 }
