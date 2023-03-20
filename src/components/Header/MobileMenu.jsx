436051955d3f030031172dc6d3a47aa0d0d3704d
AndyCambra <andycambra@gmail.com>
2023-03-20 15:48:18 -0300

    Header y Floating button mobile version

---
+++ src/components/Header/MobileMenu.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import HamButton from './HamButton'
+import { NavLink } from 'react-router-dom'
+
+const MobileMenu = ({open, location, handleClick}) => {
+  return (
+    <div className='headerMobileBox'>  
+    <NavLink to="/"> 
+            {location.pathname.startsWith('/productos/') || location.pathname.startsWith('/escuela') || location.pathname === '/landing' || location.pathname === '/prevencion-fraude' || location.pathname === '/prensa' ? (
+                <img src="/img/LogoAFZwbg.png" alt="Logo Afz" className='logoAfz' />
+            ):(             
+            <img src="/img/logoAfz.png" alt="Logo Afz" className='logoAfz-mobile' />)}
+            </NavLink>  
+          <div className= {open===true ? "navbar-open" : "out"}>
+                <NavLink to="/nosotros" className='mobile-item'>
+               NOSOTROS
+                </NavLink>
+                <NavLink to="/" className='mobile-item'>
+                PRODUCTOS
+                </NavLink>
+                <NavLink to="/" className='mobile-item'>
+                ESCUELA DE CAUCIÓN
+                </NavLink>
+                <NavLink to="/" className='mobile-item'>
+               CONTACTO
+                </NavLink>    
+          </div>
+          <div className="ham-button">
+          <HamButton open={open} handleClick={handleClick} />
+          </div>    
+        </div>
+  )
+}
+
+export default MobileMenu
