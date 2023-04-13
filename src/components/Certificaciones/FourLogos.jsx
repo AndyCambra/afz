e27fa7b57bde526bff0ad186549b357a3f283e47
AndyCambra <andycambra@gmail.com>
2023-04-13 11:26:53 -0300

    links in cerfitications y reasurers

--- src/components/Certificaciones/FourLogos.jsx
+++ src/components/Certificaciones/FourLogos.jsx
@@ -6,16 +6,16 @@ const FourLogos = () => {
     <div className='four-logos-start'> 
           
     <div className='four-logos1'>
-      <img src="img/LogoCertificaciones1.jpg" alt="ICISA"/>
+    <a href='https://icisa.org/' target="_blank" rel="noreferrer"><img src="img/LogoCertificaciones1.jpg" alt="ICISA"/></a>
     </div>
     <div className='four-logos2'>
-    <img src="img/LogoCertificaciones2.jpg" alt="Bureau Veritas"/>
+    <a href='https://www.bureauveritas.com.ar/' target="_blank" rel="noreferrer"><img src="img/LogoCertificaciones2.jpg" alt="Bureau Veritas"/></a>
     </div> 
     <div className='four-logos3'>
-    <img src="img/LogoCertificaciones3.jpg" alt="Pasa APF"/>
+    <a href='http://www.apfpasa.ch/' target="_blank" rel="noreferrer"><img src="img/LogoCertificaciones3.jpg" alt="Pasa APF"/></a>
     </div>
     <div className='four-logos4'>
-    <img src="img/LogoCertificaciones4.jpg" alt="Evaluadora Latinoamericana"/>
+    <a href='https://www.evaluadora.com/ar/resultadoCalificaciones.php?texto=AFIANZADORA+LATINOAMERICANA+C%CDA+DE+SEGUROS+S.A.%2C+Compa%F1%EDas+de+Seguros&categoria=228&empresa=100' target="_blank" rel="noreferrer"><img src="img/LogoCertificaciones4.jpg" alt="Evaluadora Latinoamericana"/></a>
     </div>
   </div>
   )
--- src/components/Nosotros/SixLogos.jsx
+++ src/components/Nosotros/SixLogos.jsx
@@ -3,33 +3,32 @@ import './sixlogos.css'
 
 const SixLogos = () => (
     <div className='six-logos-start'> 
-          
     <div className='six-logos1'>
-      <img src="img/LogosRe1.jpg" alt="Sompo Internacional"/>
+    <a href='https://www.sompo-intl.com/insurance/' target="_blank" rel="noreferrer"> <img src="img/LogosRe1.jpg" alt="Sompo Internacional"/></a>
     </div>
     <div className='six-logos2'>
-    <img src="img/LogosRe2.jpg" alt="Liberty Mutual"/>
+    <a href='https://www.libertymutual.com/' target="_blank" rel="noreferrer"> <img src="img/LogosRe2.jpg" alt="Liberty Mutual"/></a>
     </div> 
     <div className='six-logos3'>
-    <img src="img/LogosRe3.jpg" alt="Hannover re"/>
+    <a href='https://www.hannover-re.com/' target="_blank" rel="noreferrer"><img src="img/LogosRe3.jpg" alt="Hannover re"/></a>
     </div>
     <div className='six-logos4'>
-    <img src="img/LogosRe4.jpg" alt="Axis"/>
+    <a href='https://www.axiscapital.com/' target="_blank" rel="noreferrer"><img src="img/LogosRe4.jpg" alt="Axis"/></a>
     </div>
     <div className='six-logos5'>
-    <img src="img/LogosRe5.jpg" alt="Validus re"/>
+    <a href='https://www.aig.com/about-us/validus-re' target="_blank" rel="noreferrer"><img src="img/LogosRe5.jpg" alt="Validus re"/></a>
     </div>
     <div className='six-logos6'>
-    <img src="img/LogosRe6.jpg" alt="Atradius"/>
+    <a href='https://group.atradius.com/' target="_blank" rel="noreferrer"><img src="img/LogosRe6.jpg" alt="Atradius"/></a>
     </div>
     <div className='six-logos7'>
-    <img src="img/LogosRe7.jpg" alt="Coface"/>
+    <a href='https://www.coface.com/' target="_blank" rel="noreferrer"><img src="img/LogosRe7.jpg" alt="Coface"/></a>
     </div>
     <div className='six-logos8'>
-    <img src="img/LogosRe8.jpg" alt="Credendo"/>
+    <a href='https://credendo.com/en' target="_blank" rel="noreferrer"><img src="img/LogosRe8.jpg" alt="Credendo"/></a>
     </div>
     <div className='six-logos9'>
-    <img src="img/LogosRe9.jpg" alt="Hamilton"/>
+    <a href='https://www.hamiltongroup.com/' target="_blank" rel="noreferrer"><img src="img/LogosRe9.jpg" alt="Hamilton"/></a>
     </div>
    
   </div>
